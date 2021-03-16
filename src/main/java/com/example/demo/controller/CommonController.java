547fcb62d767f5e4664f8c251c7b02dbedee2a75
wangxiaomei17 <wangxiaomei8@jd.com>
2021-03-16 22:54:58 +0800

    spring boot

---
+++ src/main/java/com/example/demo/controller/CommonController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author ：wangxiaomei17
+ * @date ：Created in 2021-03-13 16:55
+ * @description：常用controller层
+ */
+
+@Controller
+public class CommonController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
