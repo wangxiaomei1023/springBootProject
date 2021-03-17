9a34d3bbb43ce0ea2cb47c07a7f6862c4e888b29
wangxiaomei17 <wangxiaomei8@jd.com>
2021-03-18 00:19:34 +0800

    spring boot aaa

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,22 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.ResourceUtils;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class DemoApplication extends WebMvcConfigurationSupport {
+public class DemoApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         System.out.println("Hello world!");
     }
-
-//    @Override
-//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX + "/static/");
-//        super.addResourceHandlers(registry);
-//    }
 
 }
